c7badfef7f3473f49aba7e173a1b7fe8c2cb51e7
Gurvir <gurvirrandhawa28@gmail.com>
2023-12-26 16:34:38 -0500

    Added image capture and incremental tile rendering
    Added image capture and incremental tile rendering

--- WFC.py
+++ WFC.py
@@ -1,32 +1,29 @@
 """ Wave Function Collapse Algorithm """
 
-import random, pygame as py
-from connectivity.T import connectivity # Swap connectivity grid: C, S, T
+import random, pygame as py, time
+from Connectivity.C import connectivity # Swap connectivity grid: C, S, T
 
-# Pygame setup
 py.init()
 py.display.set_caption("Wave Function Collapse Algorithm")
 clock = py.time.Clock()
 
 pxl = 30
-grid_w = 64
-grid_h = 36
-
-direc = "assets/30/"
-run_program = True
+grid_w, grid_h = 64, 36
+direc = "Assets/30/"
+active = True
 screen = py.display.set_mode((grid_w * pxl, grid_h * pxl))
 
 first_row = []
-for i in range(grid_w):
-    if i == 0:
-        first_tile = random.choice(list(connectivity.keys()))
+for col in range(grid_w):
+    if col == 0:
+        tile = random.choice(list(connectivity.keys()))
     else:
         entropy_tile = random.choice(list(connectivity.keys()))
-        while connectivity[first_row[i - 1]]["right"] != connectivity[entropy_tile]["left"]:
+        while connectivity[first_row[col - 1]]["right"] != connectivity[entropy_tile]["left"]:
             entropy_tile = random.choice(list(connectivity.keys()))
         else:
-            first_tile = entropy_tile
-    first_row.append(first_tile)
+            tile = entropy_tile
+    first_row.append(tile)
 
 matrix = [first_row]
 
@@ -34,32 +31,35 @@ for row in range(grid_h - 1):
     subseq_row = []
     for col in range(grid_w):
         if col == 0:
-            first_tile = random.choice(list(connectivity.keys()))
-            while connectivity[first_tile]["up"] != connectivity[matrix[row][col]]["down"]:
-                first_tile = random.choice(list(connectivity.keys()))
+            tile = random.choice(list(connectivity.keys()))
+            while connectivity[tile]["up"] != connectivity[matrix[row][col]]["down"]:
+                tile = random.choice(list(connectivity.keys()))
         else:
             entropy_tile = random.choice(list(connectivity.keys()))
             while connectivity[entropy_tile]["up"] != connectivity[matrix[row][col]]["down"] or connectivity[subseq_row[col - 1]]["right"] != connectivity[entropy_tile]["left"]:
                 entropy_tile = random.choice(list(connectivity.keys()))
             else:
-                first_tile = entropy_tile   
-        subseq_row.append(first_tile)
+                tile = entropy_tile
+        subseq_row.append(tile)
 
     matrix += [subseq_row]
-    
-while run_program:
+
+while active:
     for row in range(grid_h):
         for col in range(grid_w):
             screen.blit(py.image.load(direc + matrix[row][col] + ".png"), (col * pxl, row * pxl))
-            
+            py.display.update()
+
     for event in py.event.get():
         if event.type == py.QUIT:
-            run_program = False
+            active = False
         if event.type == py.KEYDOWN:
             if event.key == py.K_ESCAPE:
-                run_program = False
+                active = False
+            if event.key == py.K_v:
+                filename = ("Captures/Capture " + time.strftime("%Y-%H.%M.%S") + ".png")
+                py.image.save(screen, filename)
 
-    py.display.update()
     clock.tick(100)
 
 py.quit()
